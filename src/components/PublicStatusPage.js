1c2b79d93f8384167b1060b18a7ac2568537aa7e
Keshav Prajapati <keshavpj1711@gmail.com>
2025-04-13 15:24:14 +0530

    fixed a bug

--- src/components/PublicStatusPage.js
+++ src/components/PublicStatusPage.js
@@ -272,6 +272,7 @@ function PublicStatusPage() {
                       </span>
                     )}
                   </div>
+                  <IncidentTimeline incident={incident} />
                 </div>
               ))
             )}
